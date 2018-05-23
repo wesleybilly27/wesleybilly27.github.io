9711ef361e92feaaec37fcd3aaeb081942b77488
wesleybilly27 <39344880+wesleybilly27@users.noreply.github.com>
2018-05-23 13:57:32 -0300

    JOGO
    Meu primeiro jogo.

---
+++ JOGOMEU/offline.js
@@ -0,0 +1,32 @@
+﻿{
+	"version": 1527094252,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/slogo-sheet0.png",
+		"images/play-sheet0.png",
+		"images/fontecredits.png",
+		"images/ideck-sheet0.png",
+		"images/icard-sheet0.png",
+		"images/icard-sheet1.png",
+		"images/sensorcard-sheet0.png",
+		"images/bhome-sheet0.png",
+		"images/bsom-sheet0.png",
+		"images/bsom-sheet1.png",
+		"images/bmusica-sheet0.png",
+		"images/bmusica-sheet1.png",
+		"images/icoin-sheet0.png",
+		"media/draw.m4a",
+		"media/draw.ogg",
+		"media/iwan gabovitch - dark ambience loop.ogg",
+		"media/passturn.m4a",
+		"media/passturn.ogg",
+		"media/path to lake land.ogg",
+		"media/tap.m4a",
+		"media/tap.ogg",
+		"media/untap.m4a",
+		"media/untap.ogg"
+	]
+}
