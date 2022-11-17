15ddd30928d54dc901cf9684830a48aaff9382e7
wesleybilly27 <39344880+wesleybilly27@users.noreply.github.com>
2022-11-17 11:32:47 -0300

    ev
    ev

---
+++ JOGOCHARADA3.0/offline.js
@@ -0,0 +1,21 @@
+﻿{
+	"version": 1668695493,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/fundoobj-sheet0.png",
+		"images/scharada-sheet0.png",
+		"images/scharada-sheet1.png",
+		"media/021914bgm2(happytune).m4a",
+		"media/021914bgm2(happytune).ogg",
+		"media/happymusic.ogg",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
